4be868d52ebb208eea0778c13c5df103a427f47a
arthurpsribeiro <arthurpsribeiro@gmail.com>
2020-07-06 22:11:25 -0300

    site - categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -25,7 +25,9 @@ class Category extends Model {
 			":descategory"=>$this->getdescategory()
 		));
 
-		$this->setData($results[0]);
+        $this->setData($results[0]);
+        
+        Category::updateFile();
 
     }
 
@@ -51,6 +53,26 @@ class Category extends Model {
             ':idcategory'=>$this->getidcategory()
         ]);
 
+        Category::updateFile();
+
+    }
+
+    public static function updateFile()
+    {
+
+        $categories = Category::listAll();
+
+        $html = [];
+
+        foreach ($categories as $row) {
+            array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+        }
+
+        file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
+
+
+
+    
     }
 
 }
---
+++ views-cache/categories-menu.592188361181a426308d155c55946db7.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Android</a></li><li><a href="/categories/2">IOS</a></li><li><a href="/categories/4">Motorola</a></li><li><a href="/categories/3">Samsung</a></li>
