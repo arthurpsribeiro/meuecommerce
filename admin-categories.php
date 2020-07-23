8c065f4844665b408c423ad0b7179ec9a7dd3497
arthurpsribeiro <arthurpsribeiro@gmail.com>
2020-07-23 20:04:50 -0300

    admin - categorias - search & pagination

--- admin-categories.php
+++ admin-categories.php
@@ -10,13 +10,41 @@ $app->get("/admin/categories", function(){
 
     User::verifyLogin();
 
-    $categories = Category::listAll();
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 
-    $page = new PageAdmin();
+	if ($search != '') {
 
-    $page->setTpl("categories", [
-        'categories'=>$categories
-    ]);
+		$pagination = Category::getPageSearch($search, $page);
+
+	} else {
+
+		$pagination = Category::getPage($page);
+
+	}
+
+	$pages = [];
+
+	for ($x = 0; $x < $pagination['pages']; $x++)
+	{
+
+		array_push($pages, [
+			'href'=>'/admin/categories?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+			]),
+			'text'=>$x+1
+		]);
+
+	}
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		"categories"=>$pagination['data'],
+		"search"=>$search,
+		"pages"=>$pages
+	]);
 
 });
 
