0ed2c0954961a55ad4517bcdcedabfecdac9a86c
arthurpsribeiro <arthurpsribeiro@gmail.com>
2020-07-23 19:07:39 -0300

    admin - user - search & pagination

--- admin-users.php
+++ admin-users.php
@@ -7,13 +7,42 @@ $app->get("/admin/users", function(){
 
     User::verifyLogin();
 
-    $users = User::listAll();
+    $search = (isset($_GET['search'])) ? $_GET['search'] : '';
+
+    $page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+    if ($search != '') {
+
+        $pagination = User::getPageSearch($search, $page);
+
+    } else {
+
+        $pagination = User::getPage($page, 1);
+
+    }
+
+    
+    $pages = [];
+
+    for ($x = 0; $x < $pagination['pages']; $x++){
+
+        array_push($pages, [
+            'href'=>'/admin/users?'.http_build_query([
+                'page'=>$x+1,
+                'search'=>$search
+            ]),
+            'text'=>$x+1
+        ]);
+    }
 
     $page = new PageAdmin();
 
     $page->setTpl("users", array(
-        "users"=>$users
+        'users'=>$pagination['data'],
+        'search'=>$search,
+        'pages'=>$pages
     ));
+
 });
 
 $app->get("/admin/users/create", function(){
