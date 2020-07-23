9e04342f78e711dfa59d0b6686f23c66a8dbecd0
arthurpsribeiro <arthurpsribeiro@gmail.com>
2020-07-23 20:16:27 -0300

    admin - produtos - search & pagination

--- admin-products.php
+++ admin-products.php
@@ -6,15 +6,45 @@ use \Hcode\Model\Product;
 
 $app->get("/admin/products", function(){
 
-    User::verifyLogin();
+	User::verifyLogin();
 
-    $products = Product::listAll();
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 
-    $page = new PageAdmin();
+	if ($search != '') {
 
-    $page->setTpl("products", [
-        "products"=>$products
-    ]);
+		$pagination = Product::getPageSearch($search, $page);
+
+	} else {
+
+		$pagination = Product::getPage($page);
+
+	}
+
+	$pages = [];
+
+	for ($x = 0; $x < $pagination['pages']; $x++)
+	{
+
+		array_push($pages, [
+			'href'=>'/admin/products?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+			]),
+			'text'=>$x+1
+		]);
+
+	}
+
+	$products = Product::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("products", [
+		"products"=>$pagination['data'],
+		"search"=>$search,
+		"pages"=>$pages
+	]);
 
 });
 
