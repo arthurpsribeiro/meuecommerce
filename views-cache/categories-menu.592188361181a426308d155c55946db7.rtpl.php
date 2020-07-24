2b03a529868dcbfa8d2ec25f02070deb7126c170
arthurpsribeiro <arthurpsribeiro@gmail.com>
2020-07-24 10:30:56 -0300

    usuario - ajustes

--- views-cache/categories-menu.592188361181a426308d155c55946db7.rtpl.php
+++ views-cache/categories-menu.592188361181a426308d155c55946db7.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Android</a></li><li><a href="/categories/2">IOS</a></li><li><a href="/categories/4">Motorola</a></li><li><a href="/categories/3">Samsung</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Android</a></li><li><a href="/categories/6">Apple</a></li><li><a href="/categories/2">IOS</a></li><li><a href="/categories/4">Motorola</a></li><li><a href="/categories/3">Samsung</a></li>
